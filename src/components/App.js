2c941a995a45e500d05df0a84dba61ff7f89e231
Alexandre Machado <amch.smd@gmail.com>
2019-07-26 09:38:44 -0300

    Criando componentes iniciais de lista

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import './styles/App.css';
+import PokeList from './PokeList';
+import './styles/App.scss';
 
 class App extends Component {
+
+	constructor() {
+		super();
+		this.state = {};
+	}
+
   render() {
     return (
       <div className="App">
-        some pokemon stuff
+        <PokeList/>
       </div>
     );
   }
---
+++ src/components/PokeList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/PokeList.scss';
+
+// stateless component, no need to make it a class.
+const PokeList = () => {
+	return (
+		<section className="pokelist">
+		</section>
+	)
+}
+
+export default PokeList;
