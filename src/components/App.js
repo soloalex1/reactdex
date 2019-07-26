529e98a4ff3d7110988049b80eb33ab3c34e7cea
Alexandre Machado <amch.smd@gmail.com>
2019-07-25 22:15:09 -0300

    Ajustes iniciais e Sass

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        some pokemon stuff
+      </div>
+    );
+  }
+}
+
+export default App;
